8e0061cc75e901c6deec37825f1f35d81f318b72
Khishigdari <nbhishgee22@gmail.com>
2025-08-07 11:30:25 +0800

    empty input, null, icon

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-const listData = [];
+let listData = [];
 const ul = document.querySelector("ul");
 const body = document.querySelector("body");
 const input = document.createElement("input");
@@ -16,10 +16,20 @@ const render = () => {
   listData.map((element, index) => {
     const li = document.createElement("li");
 
+    // const icon = document.createElement("i"); // ======
+    // icon.classList.add("editIcon"); // ======
+
+    const iconButton = document.createElement("button"); // ======
+    iconButton.classList.add("iconButton"); // ======
+    const span = document.createElement("span"); // ======
+    span.innerText = "edit"; // ======
+    span.classList.add("material-symbols-outlined"); // ======
+
     const deleteButton = document.createElement("button");
     // const div = document.createElement("div");
     // div.classList.add("deleteContainer");
     deleteButton.innerText = "x";
+    deleteButton.classList.add("deleteButton");
 
     deleteButton.addEventListener("click", () => {
       const newListData = listData.filter((el, i) => {
@@ -28,15 +38,25 @@ const render = () => {
       listData = newListData; // <=====
       render();
     });
+
     li.innerText = element;
     // ul.appendChild(div);
+    iconButton.appendChild(span); // ======
+    li.appendChild(iconButton); // ======
     li.appendChild(deleteButton);
     ul.appendChild(li);
   });
 };
 
 function printInputValue(value) {
+  if (input.value == "") {
+    // alert("input empty");
+    // const name = prompt("hello", );
+    // console.log(name)
+    return null;
+  }
   listData.push(value);
+  input.value = "";
   // const li = document.createElement("li");
   // li.innerText = value;
   // ul.appendChild(li);
@@ -46,8 +66,12 @@ function printInputValue(value) {
 }
 
 button.addEventListener("click", () => {
+  // if (input.value.length === 0) {
+  //   deleteButton.disabled = true;
+  // } // ======
   printInputValue(input.value);
   render();
+  // input.value = ""; // ======
 });
 
 // div.appendChild(ul);
